5e7eea0a976ab5c0fd5baa21a724819f96688dce
Angel Lootus <Angel.Lootus@enefit.ee>
2025-01-29 11:10:56 +0200

    update tailwind setup, add dashboard button event tracking

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { analytics } from "../firebase";
 import { logEvent } from "firebase/analytics";
 
 export default function DashboardView() {
+    const [isClicked, setIsClicked] = useState(false);
 
     useEffect(() => {
         logEvent(analytics, "dashboard_view", {
@@ -10,10 +11,25 @@ export default function DashboardView() {
           timestamp: new Date().toISOString(),
         });
     }, []);
+
+    const handleDashboardAction= async () => {
+        logEvent(analytics, "button_click", {
+            button_name: "Dashboard action",
+            timestamp: new Date().toISOString(),
+        });
+        setIsClicked(prevIsCliked => !prevIsCliked)
+    };
     
     return (
         <>
             <h1 className="text-center text-5xl mt-80">Dashboard View</h1>
+            {isClicked&&<h4 className="text-center text-5xl mt-10">button clicked</h4>}
+            <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-2xl hover:bg-blue-600 mt-10 mx-auto block"
+                onClick={handleDashboardAction}
+            >
+               Some Action
+            </button>
         </>
     )
 }
